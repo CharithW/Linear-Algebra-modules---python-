5316f46f51dc2fe7055dc59824b1c59a4739378d
CharithW <yashoda.charith10@gmail.com>
2016-07-15 10:56:45 +0530

    Vector PLUS , MINUS , and SCALAR MULTIPLY

--- Vector.py
+++ Vector.py
@@ -1,4 +1,16 @@
 class Vector(object):
+    def plus(self , v):
+        new_coordinates = [x+y for x,y in zip(self.coordinates , v.coordinates)]
+        return Vector(new_coordinates)
+    
+    def minus(self , v):
+        new_coordinates = [x-y for x,y in zip(self.coordinates , v.coordinates)]
+        return Vector(new_coordinates)
+    
+    def times_scalar(self , c ):
+        new_coordinates = [c*x for x in self.coordinates]
+        return Vector(new_coordinates)
+    
     def __init__(self , coordinates):
         try :
             if not coordinates :
@@ -8,6 +20,7 @@ class Vector(object):
             
         except ValueError:
             raise ValueError('The coordinates can not be empty')
+        
         
         except TypeError:
             raise TypeError('coordinates must be iterable')
@@ -19,6 +32,8 @@ class Vector(object):
         return self.coordinates == v.coordinates 
     
 
-vec = Vector([2,2,3])
-print vec
+v = Vector([8.218 , -9.341])
+w = Vector([-8.223 ,0.878])
+
+print v.plus(w)
 
